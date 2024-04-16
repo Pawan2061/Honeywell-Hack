e712da0262d2e02dc4330c4b41f0bbadbe5cfb8a
devsargam <sargampoudel100@gmail.com>
2024-04-17 04:41:58 +0530

    feat: fix hero padding

--- src/components/hero/hero-main.tsx
+++ src/components/hero/hero-main.tsx
@@ -16,7 +16,7 @@ export function MainHero() {
           }}
         />
       </div>
-      <div className="mx-auto max-w-2xl py-32 sm:py-48 lg:py-56">
+      <div className="mx-auto max-w-2xl py-16 sm:py-24 lg:py-32">
         <div className="hidden sm:mb-8 sm:flex sm:justify-center">
           <div className="relative rounded-full px-3 py-1 text-sm leading-6 text-gray-600 ring-1 ring-gray-900/10 hover:ring-gray-900/20">
             Our entry for honeywell hackathon
